c55fc0e304cfb22ccc8d0eac3aff75cefeb5cade
dawun <dawun@lacy.co.kr>
2024-05-14 13:20:36 +0900

    test

--- app/Router/Auth/AuthRouter.py
+++ app/Router/Auth/AuthRouter.py
@@ -1,5 +1,6 @@
 import os
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
+from fastapi.responses import RedirectResponse
 from Service.Auth.GoogleOAuth import *
 
 google = APIRouter(prefix='/auth/google')
@@ -10,9 +11,9 @@ async def getUrl():
     return getOAuthUrl()
 
 
-@google.get('/callback', tags=['auth'])
-async def callback(code: str):
+@google.get('/callback', tags=['auth'], response_class=RedirectResponse)
+async def callback(code: str, response: Response):
     jwtToken = authGoogle(code)
-    response = RedirectResponse(f"{os.getenv('FRONT_HOST')}/")
-    response.set_cookie(key="authorization", value=jwtToken)
-    return response
+    response.set_cookie(key="authorization", value=jwtToken, domain=f"{os.getenv('FRONT_HOST')}/")
+    response.set_cookie(key="authorization", value=jwtToken, domain=f"{os.getenv('API_HOST')}/")
+    return f"{os.getenv('FRONT_HOST')}/"
