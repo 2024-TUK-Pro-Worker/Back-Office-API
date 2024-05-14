5de2bc332b74443525e576260e1e4690c5212e5b
dawun <dawun@lacy.co.kr>
2024-05-14 12:28:40 +0900

    테스트 수정

--- app/Router/Auth/AuthRouter.py
+++ app/Router/Auth/AuthRouter.py
@@ -11,9 +11,10 @@ async def getUrl():
     return getOAuthUrl()
 
 
-@google.get('/callback', tags=['auth'])
+@google.get('/callback', tags=['auth'], response_class=RedirectResponse)
 async def callback(code: str):
     jwtToken = authGoogle(code)
-    response = RedirectResponse(f"{os.getenv('FRONT_HOST')}/")
-    response.set_cookie(key="authorization", value=jwtToken)
+    response = RedirectResponse(url=f"{os.getenv('FRONT_HOST')}/")
+    response.set_cookie(key="authorization", value=jwtToken, domain=f"{os.getenv('FRONT_HOST')}")
+    response.set_cookie(key="authorization", value=jwtToken, domain=f"{os.getenv('API_HOST')}")
     return response
