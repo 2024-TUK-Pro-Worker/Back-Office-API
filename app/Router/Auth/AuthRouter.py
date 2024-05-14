e111a1ef38703297b9984495fc730c240ec17834
dawun <dawun@lacy.co.kr>
2024-05-14 14:02:57 +0900

    test

--- app/Router/Auth/AuthRouter.py
+++ app/Router/Auth/AuthRouter.py
@@ -14,6 +14,5 @@ async def getUrl():
 async def callback(code: str):
     jwtToken = authGoogle(code)
     response = RedirectResponse(f"https://{os.getenv('FRONT_HOST')}/")
-    response.set_cookie(key="authorization", value=jwtToken, path="/", domain=f"{os.getenv('API_HOST')}", secure=True, httponly=True, samesite="none")
-    response.set_cookie(key="authorization", value=jwtToken, path="/", domain=f"{os.getenv('FRONT_HOST')}", secure=True, httponly=True, samesite="none")
+    response.set_cookie(key="authorization", value=jwtToken, path="/", domain=f"{os.getenv('HOST')}")
     return response
