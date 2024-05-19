f9e2737e9f1984839020432e3747d1ce449a2f54
dawun <dawun@lacy.co.kr>
2024-05-19 15:56:48 +0900

    기본 프롬프트 수정

--- app/Model/Account/Prompt.py
+++ app/Model/Account/Prompt.py
@@ -19,7 +19,7 @@ class Prompt:
         except:
             return {
                 'uuid': uuid,
-                'content': '재미있고, 흥미진진한 드라마 시나리오를 작성해줘. 시나리오는 씬별로 구분 되어해. 씬은 7개를 만들어줘. 시나리오에는 제목, 등장인물에 대한 이름, 씬별 등장인물 대사가 있어야해.'
+                'content': '재미있고, 흥미진진한 드라마 시나리오를 작성해줘. 시나리오는 씬별로 구분 되어해. 씬은 7개를 만들어줘.'
             }
         finally:
             self.db.close()
@@ -27,7 +27,7 @@ class Prompt:
     def insertDefaultPrompt(self, uuid):
         try:
             self.db.add(Models.Prompt(uuid=uuid,
-                                      content='재미있고, 흥미진진한 드라마 시나리오를 작성해줘. 시나리오는 씬별로 구분 되어해. 씬은 7개를 만들어줘. 시나리오에는 제목, 등장인물에 대한 이름, 씬별 등장인물 대사가 있어야해.'))
+                                      content='재미있고, 흥미진진한 드라마 시나리오를 작성해줘. 시나리오는 씬별로 구분 되어해. 씬은 7개를 만들어줘.'))
             self.db.commit()
             return True
         except:
