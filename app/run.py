459b310084f884fa8928a8e8a83f163bf350a50c
dawun <dawun@lacy.co.kr>
2024-05-25 16:53:59 +0900

    cors 수정

--- app/run.py
+++ app/run.py
@@ -43,15 +43,16 @@ app.add_middleware(
 async def add_process_time_header(request: Request, call_next):
     try:
         if '/auth/' not in request.url.path:
-            if request.cookies.get('authorization') is None and request.method != 'OPTIONS':
-                return responses.JSONResponse({
-                    'result': 'fail',
-                    'message': '403 Forbidden'
-                }, status_code=403)
+            if request.method != 'OPTIONS':
+                if request.cookies.get('authorization') is None:
+                    return responses.JSONResponse({
+                        'result': 'fail',
+                        'message': '403 Forbidden'
+                    }, status_code=403)
 
-            # JWT 정상 변경 가능 여부 확인
-            jwtToken = request.cookies.get('authorization')
-            jwt.decode(jwtToken, os.getenv('JWT_SALT_KEY'), algorithms="HS256")
+                # JWT 정상 변경 가능 여부 확인
+                jwtToken = request.cookies.get('authorization')
+                jwt.decode(jwtToken, os.getenv('JWT_SALT_KEY'), algorithms="HS256")
 
         start_time = time.time()
         response = await call_next(request)
